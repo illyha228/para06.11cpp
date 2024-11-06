55c5a1086a7eb1f7b2453385903788d6d61485d0
illya <korsillua@gmail.com>
2024-11-06 20:10:03 +0200

    new

--- main.cpp
+++ main.cpp
@@ -1,39 +1,15 @@
 ﻿#include <iostream>
-using namespace std;
+#include <Windows.h>
 
-int main() {
-    double num1, num2;
-    char oper;
+using std::cout;
+using std::endl;
 
-    cout << "1 chislo vvedit: ";
-    cin >> num1;
-    cout << "vvedit diu (+, -, *, /): ";
-    cin >> oper;
-    cout << "2 chislo vvedit: ";
-    cin >> num2;
 
-    switch (oper) {
-    case '+':
-        cout << "rezultat: " << num1 + num2 << endl;
-        break;
-    case '-':
-        cout << "rezultat: " << num1 - num2 << endl;
-        break;
-    case '*':
-        cout << "rezultat: " << num1 * num2 << endl;
-        break;
-    case '/':
-        if (num2 != 0) {
-            cout << "rezultat: " << num1 / num2 << endl;
-        }
-        else {
-            cout << "Errorrr: //0!" << endl;
-        }
-        break;
-    default:
-        cout << "Error dia!" << endl;
-        break;
-    }
-
-    return 0;
+int main() 
+{
+	GetConsoleCP(1251);
+	system("chcp 1231");
+	cout << "Привіт Гудзиик" << endl;
+	system("pause");
+	return 0;
 }
